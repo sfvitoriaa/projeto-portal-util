735ab862ea486dbd2f861b0d853956b35ec2fd86
sfvitoriaa <0001046198@senaimgaluno.com.br>
2026-02-25 19:35:48 -0300

    email

--- portal/src/schemas/contactSchema.ts
+++ portal/src/schemas/contactSchema.ts
@@ -2,7 +2,7 @@ import { z } from "zod";
 
 export const contactSchema = z.object({
   name: z.string().min(3, "Nome deve ter pelo menos 3 caracteres"),
-  email: z.string().email("Email inválido"),
+  email: z.email("Email inválido"),
   phone: z.string().regex(/^\d+$/, "Telefone deve conter apenas números"),
 });
 
